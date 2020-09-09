4e59c5bf925090f1c1b191a8346f62bec7399fd0
Ferraz Junior, N <norisjunior@gmail.com>
2020-09-09 17:54:40 -0300

    update make

--- cloud-platform/new-provisioning.sh
+++ cloud-platform/new-provisioning.sh
@@ -93,6 +93,10 @@ curl -iX POST \
 sleep 1;
 
 
+###########################################################################
+######    CWIoT suffix: 00124b05257a (Native)                       #######
+###########################################################################
+
 echo "\n###### SENSORTAG CC2650 TARGET CWIoT\n"
 
 curl -iX POST \
@@ -131,6 +135,11 @@ curl -iX POST \
 
 sleep 1;
 
+
+
+###########################################################################
+######    CWIoT suffix: 00124b4a527d (Native)                       #######
+###########################################################################
 
 echo "\n###### REMOTE CC2538 TARGET CWIoT\n"
 
@@ -171,275 +180,6 @@ curl -iX POST \
 sleep 1;
 
 
-echo "\n###### LAUNCHPAD1 CC2650 TARGET CWIoT\n"
-
-curl -iX POST \
-  'http://localhost:4041/iot/devices' \
-  -H 'Content-Type: application/json' \
-  -H 'fiware-service: school' \
-  -H 'fiware-servicepath: /fd00' \
-  -d '{
- "devices": [
-   {
-     "device_id":   "00124b82ad03",
-     "entity_name": "00124b82ad03",
-     "entity_type": "Launchpad",
-     "protocol":    "PDI-IoTA-UltraLight",
-     "transport":   "MQTT",
-     "timezone":    "Europe/Berlin",
-     "commands": [
-       {"name": "33030","type": "command"},
-       {"name": "33040","type": "command"},
-       {"name": "33110","type": "command"},
-       {"name": "33111","type": "command"},
-       {"name": "33380","type": "command"}
-      ],
-      "attributes": [
-       {"object_id": "33030", "name": "33030", "type": "actual_temp"},
-       {"object_id": "33040", "name": "33040", "type": "actual_hum"}
-      ],
-      "static_attributes": [
-        {"name":"refStore", "type": "Relationship","value": "urn:ngsi-ld:School:001"}
-     ]
-   }
- ]
-}
-'
-
-
-
-sleep 1;
-
-
-
-echo "\n###### LAUNCHPAD2 CC2650 TARGET CWIoT\n"
-
-curl -iX POST \
-  'http://localhost:4041/iot/devices' \
-  -H 'Content-Type: application/json' \
-  -H 'fiware-service: school' \
-  -H 'fiware-servicepath: /fd00' \
-  -d '{
- "devices": [
-   {
-     "device_id":   "00124b82b206",
-     "entity_name": "00124b82b206",
-     "entity_type": "Launchpad",
-     "protocol":    "PDI-IoTA-UltraLight",
-     "transport":   "MQTT",
-     "timezone":    "Europe/Berlin",
-     "commands": [
-       {"name": "33030","type": "command"},
-       {"name": "33040","type": "command"},
-       {"name": "33110","type": "command"},
-       {"name": "33111","type": "command"},
-       {"name": "33380","type": "command"}
-      ],
-      "attributes": [
-       {"object_id": "33030", "name": "33030", "type": "actual_temp"},
-       {"object_id": "33040", "name": "33040", "type": "actual_hum"}
-      ],
-      "static_attributes": [
-        {"name":"refStore", "type": "Relationship","value": "urn:ngsi-ld:School:001"}
-     ]
-   }
- ]
-}
-'
-sleep 1;
-
-
-
-echo "\n###### Windows WiFi standalone app\n"
-
-curl -iX POST \
-  'http://localhost:4041/iot/devices' \
-  -H 'Content-Type: application/json' \
-  -H 'fiware-service: school' \
-  -H 'fiware-servicepath: /fd00' \
-  -d '{
- "devices": [
-   {
-     "device_id":   "0012e3034d8c",
-     "entity_name": "0012e3034d8c",
-     "entity_type": "WindowsWiFi",
-     "protocol":    "PDI-IoTA-UltraLight",
-     "transport":   "MQTT",
-     "timezone":    "Europe/Berlin",
-     "commands": [
-       {"name": "33030","type": "command"},
-       {"name": "33040","type": "command"},
-       {"name": "33110","type": "command"},
-       {"name": "33111","type": "command"},
-       {"name": "33380","type": "command"}
-      ],
-      "attributes": [
-       {"object_id": "33030", "name": "33030", "type": "actual_temp"},
-       {"object_id": "33040", "name": "33040", "type": "actual_hum"}
-      ],
-      "static_attributes": [
-        {"name":"refStore", "type": "Relationship","value": "urn:ngsi-ld:School:001"}
-     ]
-   }
- ]
-}
-'
-sleep 1;
-
-
-
-# Raspberry pi: 'b8:27:eb:00:f6:d0'
-#client_id: 0012eb00f6d0
-
-curl -iX POST \
-  'http://localhost:4041/iot/devices' \
-  -H 'Content-Type: application/json' \
-  -H 'fiware-service: school' \
-  -H 'fiware-servicepath: /fd00' \
-  -d '{
- "devices": [
-   {
-     "device_id":   "0012eb00f6d0",
-     "entity_name": "0012eb00f6d0",
-     "entity_type": "Raspberry",
-     "protocol":    "PDI-IoTA-UltraLight",
-     "transport":   "MQTT",
-     "timezone":    "Europe/Berlin",
-     "commands": [
-       {"name": "33030","type": "command"},
-       {"name": "33040","type": "command"},
-       {"name": "33110","type": "command"},
-       {"name": "33111","type": "command"},
-       {"name": "33380","type": "command"}
-      ],
-      "attributes": [
-       {"object_id": "33030", "name": "33030", "type": "actual_temp"},
-       {"object_id": "33040", "name": "33040", "type": "actual_hum"}
-      ],
-      "static_attributes": [
-        {"name":"refStore", "type": "Relationship","value": "urn:ngsi-ld:School:001"}
-     ]
-   }
- ]
-}
-'
-
-sleep 1;
-
-
-###########################################################################
-###########################################################################
-#### Dummy test measurements (for testing purposes): ######################
-###########################################################################
-###########################################################################
-
-echo "#### Mosquitto dummy temperatura sensortag ####"
-mosquitto_pub -h localhost -t /99/00124b05257a -m '033030|23.123'
-
-sleep 1;
-
-echo "#### Mosquitto dummy umidade ####"
-mosquitto_pub -h localhost -t /99/00124b05257a -m '033040|80.789'
-
-sleep 1;
-
-echo "#### Mosquitto dummy RedLed ####"
-mosquitto_pub -h localhost -t /99/00124b05257a -m '033110|1'
-
-sleep 1;
-
-echo "#### Mosquitto dummy GreenLed ####"
-mosquitto_pub -h localhost -t /99/00124b05257a -m '033111|1'
-
-
-
-
-echo "#### Mosquitto dummy temperatura remote ####"
-mosquitto_pub -h localhost -t /99/00124b4a527d -m '033030|23.123'
-
-sleep 1;
-
-echo "#### Mosquitto dummy temperatura native ####"
-mosquitto_pub -h localhost -t /99/010203060708 -m '033030|23.123'
-
-sleep 1;
-
-echo "#### Mosquitto dummy temperatura launchpad1 ####"
-mosquitto_pub -h localhost -t /99/00124b82ad03 -m '033030|23.123'
-
-sleep 1;
-
-echo "#### Mosquitto dummy temperatura launchpad2 ####"
-mosquitto_pub -h localhost -t /99/00124b82b206 -m '033030|23.123'
-
-sleep 1;
-
-
-#########################################################################################
-#################################                        ################################
-###################################                    ##################################
-#################################### FIWARE - Cygnus ####################################
-###################################                    ##################################
-#################################                        ################################
-#########################################################################################
-
-##################
-##### Subscription
-
-echo "###### Cygnus - Subscription - VER ISSO!"
-
-curl -iX POST \
-  'http://localhost:1026/v2/subscriptions' \
-  -H 'Content-Type: application/json' \
-  -H 'fiware-service: school' \
-  -H 'fiware-servicepath: /fd00' \
-  -d '{
-  "description": "Notify Cygnus of all context changes",
-  "subject": {
-    "entities": [
-      {
-        "idPattern": ".*"
-      }
-    ]
-  },
-  "notification": {
-    "http": {
-      "url": "http://cygnus:5050/notify"
-    },
-    "metadata": ["dateCreated", "dateModified"],
-    "attrsFormat": "legacy"
-  }
-}'
-
-
-sleep 1;
-
-
-##################
-##### Subscribe check
-
-echo "############################################################"
-echo "############################################################"
-echo "########################## CHECKS ##########################"
-echo "############################################################"
-echo "############################################################"
-
-sleep 1;
-
-echo "############################################################"
-echo "################### Cygnus Subscription ####################"
-echo "############################################################"
-
-curl -X GET \
-  'http://localhost:1026/v2/subscriptions' \
-  -H 'fiware-service: lwpubsub_schoolA' \
-  -H 'fiware-servicepath: /fd00' | python -m json.tool
-
-
-
-sleep 1;
-
-
 echo "############################################################"
 echo "############                                    ############"
 echo "###########                                      ###########"
@@ -448,14 +188,6 @@ echo "###########                                      ###########"
 echo "############                                    ############"
 echo "############################################################"
 
-###################
-####### Checks
-
-#Native
-# curl -X GET \
-#   'http://localhost:1026/v2/entities/010203060708?type=LWPUBSUB&options=keyValues' \
-#   -H 'fiware-service: school' \
-#   -H 'fiware-servicepath: /fd00' | python -m json.tool
 
 sleep 1;
 
@@ -474,6 +206,17 @@ curl -X GET \
 sleep 1;
 
 
+curl -X GET \
+  'http://localhost:1026/v2/entities/00124b4a527d?type=Remote&options=keyValues' \
+  -H 'fiware-service: school' \
+  -H 'fiware-servicepath: /fd00' | python -m json.tool
+
+sleep 1;
+
+
+
+
+
 echo "############################################################"
 echo "############                                    ############"
 echo "###########                                      ###########"
@@ -482,31 +225,9 @@ echo "###########                                      ###########"
 echo "############                                    ############"
 echo "############################################################"
 
-
-
-
-curl -iX POST \
-  http://localhost:4041/v2/op/update \
-  -H 'Content-Type: application/json' \
-  -H 'fiware-service: school' \
-  -H 'fiware-servicepath: /fd00' \
-  -d '{
-    "actionType": "update",
-    "entities": [
-        {
-            "type": "Native",
-            "id": "010203060708",
-            "33110" : {
-                "type": "command",
-                "value": "0"
-            }
-        }
-    ]
-}'
-
 sleep 1;
 
-echo -e "RED LED:"
+echo -e "Turn on RED LED:"
 curl -iX POST \
   http://localhost:4041/v2/op/update \
   -H 'Content-Type: application/json' \
@@ -529,7 +250,7 @@ curl -iX POST \
 
 
 
-echo -e "GREEN LED:"
+echo -e "Turn on GREEN LED:"
 curl -iX POST \
   http://localhost:4041/v2/op/update \
   -H 'Content-Type: application/json' \
@@ -548,58 +269,6 @@ curl -iX POST \
         }
     ]
 }'
-
-
-
-
-
-
-echo -e "Alarm on:"
-curl -iX POST \
-  http://localhost:4041/v2/op/update \
-  -H 'Content-Type: application/json' \
-  -H 'fiware-service: school' \
-  -H 'fiware-servicepath: /fd00' \
-  -d '{
-    "actionType": "update",
-    "entities": [
-        {
-            "type": "Sensortag",
-            "id": "00124b05257a",
-            "33380" : {
-                "type": "command",
-                "value": "1"
-            }
-        }
-    ]
-}'
-
-sleep 4;
-
-echo -e "Alarm off:"
-curl -iX POST \
-  http://localhost:4041/v2/op/update \
-  -H 'Content-Type: application/json' \
-  -H 'fiware-service: school' \
-  -H 'fiware-servicepath: /fd00' \
-  -d '{
-    "actionType": "update",
-    "entities": [
-        {
-            "type": "Sensortag",
-            "id": "00124b05257a",
-            "33380" : {
-                "type": "command",
-                "value": "0"
-            }
-        }
-    ]
-}'
-
-
-
-
-
 
 
 
@@ -624,161 +293,3 @@ curl -iX POST \
         }
     ]
 }'
-
-
-
-# echo ""
-# echo "Grafana info:"
-# echo "http://localhost:3003/login"
-# echo "user: admin"
-# echo "senha: admin"
-# echo ""
-# echo "Add datasource: MySQL"
-# echo "host: mysql-db:3306"
-# echo "Database: lwpubsub_schoola"
-# echo "User: root"
-# echo "Pass: 123"
-# echo ""
-# echo ""
-# echo "###### Example Grafana queries:"
-# echo ""
-# echo "###Temp"
-# echo "SELECT UNIX_TIMESTAMP(recvtime) as time_sec, CAST((attrvalue) AS DECIMAL(7,3)) as value, 'temperatura' as metric FROM \`fd00_010203060708_LWPUBSUB\` WHERE attrname ='33030' ORDER BY time_sec;"
-# echo "###RedLed"
-# echo "SELECT UNIX_TIMESTAMP(recvtime) as time_sec, CAST((attrvalue) AS UNSIGNED) as value, 'RedLED' as metric FROM \`fd00_010203060708_LWPUBSUB\` WHERE attrname ='33030' ORDER BY time_sec;"
-# echo ""
-
-
-
-
-
-#################################################################################################
-# FIWARE TUTORIAL:
-# curl -iX POST \
-#   'http://localhost:4041/iot/services' \
-#   -H 'Content-Type: application/json' \
-#   -H 'fiware-service: openiot' \
-#   -H 'fiware-servicepath: /' \
-#   -d '{
-#  "services": [
-#    {
-#      "apikey":      "4jggokgpepnvsb2uv4s40d59ov",
-#      "cbroker":     "http://orion:1026",
-#      "entity_type": "Thing",
-#      "resource":    ""
-#    }
-#  ]
-# }'
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-# curl -iX POST \
-#   'http://localhost:4041/iot/devices' \
-#   -H 'Content-Type: application/json' \
-#   -H 'fiware-service: openiot' \
-#   -H 'fiware-servicepath: /' \
-#   -d '{
-#  "devices": [
-#    {
-#      "device_id":   "motion001",
-#      "entity_name": "urn:ngsi-ld:Motion:001",
-#      "entity_type": "Motion",
-#      "protocol":    "PDI-IoTA-UltraLight",
-#      "transport":   "MQTT",
-#      "timezone":    "Europe/Berlin",
-#      "attributes": [
-#        { "object_id": "c", "name": "count", "type": "Integer" }
-#      ],
-#      "static_attributes": [
-#        { "name":"refStore", "type": "Relationship", "value": "urn:ngsi-ld:Store:001"}
-#      ]
-#    }
-#  ]
-# }
-# '
-#
-#
-#
-#
-#
-# mosquitto_pub -m "c|1" -t "/4jggokgpepnvsb2uv4s40d59ov/motion001/attrs"
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-# curl -X GET \
-#   'http://localhost:1026/v2/entities/urn:ngsi-ld:Motion:001?type=Motion' \
-#   -H 'fiware-service: openiot' \
-#   -H 'fiware-servicepath: /' | python -m json.tool
-#
-#
-#
-#
-#
-#
-#
-#   curl -iX POST \
-#   'http://localhost:4041/iot/devices' \
-#   -H 'Content-Type: application/json' \
-#   -H 'fiware-service: openiot' \
-#   -H 'fiware-servicepath: /' \
-#   -d '{
-#   "devices": [
-#     {
-#       "device_id": "bell001",
-#       "entity_name": "urn:ngsi-ld:Bell:001",
-#       "entity_type": "Bell",
-#       "protocol": "PDI-IoTA-UltraLight",
-#       "transport": "MQTT",
-#       "commands": [
-#         { "name": "ring", "type": "command" }
-#        ],
-#        "static_attributes": [
-#          {"name":"refStore", "type": "Relationship","value": "urn:ngsi-ld:Store:001"}
-#       ]
-#     }
-#   ]
-# }
-# '
-#
-#
-#
-#
-#
-#
-#
-#
-#
-# curl -iX POST \
-#   http://localhost:4041/v2/op/update \
-#   -H 'Content-Type: application/json' \
-#   -H 'fiware-service: openiot' \
-#   -H 'fiware-servicepath: /' \
-#   -d '{
-#     "actionType": "update",
-#     "entities": [
-#         {
-#             "type": "Bell",
-#             "id": "urn:ngsi-ld:Bell:001",
-#             "ring" : {
-#                 "type": "command",
-#                 "value": ""
-#             }
-#         }
-#     ]
-# }'
--- lwpubsub-system/make_lwpubsub_sensortag-cloud_cwiot.sh
+++ lwpubsub-system/make_lwpubsub_sensortag-cloud_cwiot.sh
@@ -8,7 +8,6 @@ if [[ $# -eq 0 ]] ; then
     echo 'usage: provide:'
     echo '- crypto algorithm .: CBC, or CTR (case sensitive)'
     echo '- crypto key .......: 128, 192, or 256)'
-    echo '- TSCH schedule ....: MINIMAL or ORCHESTRA'
     echo ''
     echo 'Poll frequency fixed at 1 message each 30 seconds'
     echo ''
@@ -31,30 +30,7 @@ if   [ "$2" != '128' ] && [ "$2" != '192' ] && [ "$2" != '256' ] ; then
   exit 0
 fi
 
-if   [ "$3" != 'MINIMAL' ] && [ "$3" != 'ORCHESTRA' ] ; then
-  echo 'error: only MINIMAL or ORCHESTRA allowed TSCH schedules'
-  exit 0
-fi
-
-if   [ "$3" == 'ORCHESTRA' ] ; then
-  is_orchestra=1
-fi
-
-
-
-#dir_binaries=~/Google\ Drive/Doutorado/#Artigos/LWPubSub\ for\ IoT/Experiments/binaries
-#dir_footprint=~/Google\ Drive/Doutorado/#Artigos/LWPubSub\ for\ IoT/Experiments/footprint
-#file_flashprof=$dir_footprint"/lwsec-pubsub-system-$4-AES-$1-$2-$3-flashprof-sensortag.log"
-#file_ramprof=$dir_footprint"/lwsec-pubsub-system-$4-AES-$1-$2-$3-ramprof-sensortag.log"
-
-#make distclean
-
-#make lwsec-pubsub-system.flashprof TARGET=cc26x0-cc13x0 BOARD=sensortag/cc2650 MAKE_KEYSIZE="$2" MAKE_CRYPTOMODE="$1" > "$file_flashprof"
-
-#make lwsec-pubsub-system.ramprof TARGET=cc26x0-cc13x0 BOARD=sensortag/cc2650 MAKE_KEYSIZE="$2" MAKE_CRYPTOMODE="$1" > "$file_ramprof"
 
 make distclean
 
-make TARGET=cc26x0-cc13x0 BOARD=sensortag/cc2650 MAKE_KEYSIZE="$2" MAKE_CRYPTOMODE="$1" MAKE_WITH_ENERGY=1 MAKE_WITHSCHEDORCHESTRA=$is_orchestra
-
-#cp build/cc26x0-cc13x0/sensortag/cc2650/lwsec-pubsub-system.hex "$dir_binaries/lwsec-pubsub-system-Cloud_CWIoT-AES-$1-$2-$3.hex"
+make TARGET=cc26x0-cc13x0 BOARD=sensortag/cc2650 MAKE_KEYSIZE="$2" MAKE_CRYPTOMODE="$1" MAKE_WITH_ENERGY=1
